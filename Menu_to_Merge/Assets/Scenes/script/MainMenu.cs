5b8eecc3d4c2391780976467e2404eab3321ceb9
AUenz <auenzelm@smail.uni-koeln.de>
2017-06-02 16:30:44 +0200

    master

---
+++ Menu_to_Merge/Assets/Scenes/script/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    private Transform cameraTransform;
+    private Transform cameraDesiredLookAt;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+
+    public void LookAtMenu(Transform menuTransform)
+    {
+        //Camera.main.transform.LookAt(menuTransform.position);
+        cameraDesiredLookAt = menuTransform;
+    }
+
+    private void Update()
+    {
+        if (cameraDesiredLookAt != null)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, 4 * Time.deltaTime);
+        }
+    }
+}
